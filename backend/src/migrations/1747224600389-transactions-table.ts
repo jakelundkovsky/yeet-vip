9721d7cc9ea0b3124542360bd73948f0a1bac3b5
jakelundkovsky <jake.lundkovsky@gmail.com>
2025-05-14 07:16:12 -0500

    txns table migration

---
+++ backend/src/migrations/1747224600389-transactions-table.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TransactionsTable1747224600389 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS "transactions" (
+                id UUID PRIMARY KEY,
+                amount DECIMAL NOT NULL,
+                user_id UUID NOT NULL,
+                created_at TIMESTAMP NOT NULL DEFAULT now(),
+                updated_at TIMESTAMP NOT NULL DEFAULT now(),
+
+                CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
